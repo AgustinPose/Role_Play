327837c96398dc8541762ffcba2c81951efdb360
Manuela Guedez <manuela.guedez@correo.ucu.edu.uy>
2023-09-16 08:50:26 -0300

    Métodos de Elf que hablamos en clase sin la lógica

---
+++ src/Library/Elf.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Library
+{
+    public class Elf
+    {
+        private double lives = 100;
+        public double Lives { get => lives; }
+        public string Name { get; set;}
+        public ArrayList Items { get; set; }
+
+        public Elf (string name, ArrayList items)
+        {
+            this.Name = name;
+            this.Items = items;
+        }
+        
+        public void AddItem(object newItem)
+        {
+            Items.Add(newItem);
+        }
+
+        public void RemoveItem(object item)
+        {
+            Items.Remove(item);
+        }
+
+        public void GetAttackValue()
+        {
+
+        }
+
+        public void GetDefenseValue()
+        {
+
+        }
+
+
+        public void AttackWizard(object wizard)
+        {
+            // tinen que estar las otras clases creadas
+        }
+
+        public void AttackElf(Elf elf)
+        {
+            if (! ReferenceEquals(this, elf))
+            {
+
+            }
+        }
+
+        public void AttackDwarf(object dwarf)
+        {
+
+        }
+
+        public void Heal(int heal)
+        {
+            if (lives + heal <= 100)
+            {
+                lives += heal;
+            }
+        }
+    }
+}

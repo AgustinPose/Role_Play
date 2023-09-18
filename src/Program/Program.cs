3e9cfbdc965ff734e92d530e8deeeb5fb77f69dc
AgustinPose <poseagus15@gmail.com>
2023-09-17 22:11:05 -0300

    Manuela Pesada

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,12 +11,13 @@ namespace Program
             Item sword = new(50, 0);
             Item armor = new(0, 30);
             Item swordSuprema = new(80, 0);
+            Item armorLegendaria = new(0, 60);
             
             List<Item> itemsManu = new List<Item> {sword, armor};
             Elf elfo = new("Manu", itemsManu);
 
             
-            List<Item> itemsAgus = new List<Item> {swordSuprema, armor};
+            List<Item> itemsAgus = new List<Item> {swordSuprema, armorLegendaria};
             Dwarf dwarf  = new("Agus", itemsAgus);
         }
     }
